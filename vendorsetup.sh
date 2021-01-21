2c2a9755625864e98ee447dfef53d0e040ab1406
ElectroPerf <kunmun.devroms@gmail.com>
2021-01-21 18:42:08 +0530

    Changed to LOS Hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,8 @@
 export SELINUX_IGNORE_NEVERALLOWS=true
 rm -rf vendor/qcom/opensource/power
+rm -rf hardware/qcom-caf/msm8998
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
