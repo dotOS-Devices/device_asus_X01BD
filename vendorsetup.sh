afe1128451f59cb8e456f9bdd2f8c61e66f0c71e
Curious-To-Learn <alokpravabarik@gmail.com>
2021-02-22 10:35:11 +0000

    Track Addon Repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,4 @@ git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://gitlab.com/Curious-To-Learn/vendor_addons_oneplus_apps.git -b OOS11 vendor/addons/oneplus/apps
